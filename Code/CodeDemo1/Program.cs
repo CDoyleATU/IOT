7a3590d99e86c8f2a8b5d69d68a97a20e91b3f2f
CDoyleATU <123488461+CDoyleATU@users.noreply.github.com>
2023-03-09 15:10:02 +0000

    First Code Demos

---
+++ Code/CodeDemo1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CodeDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coins are entered into device
+            // Reads size of coin
+
+            Console.WriteLine("Coin entered: ");
+            double coin = double.Parse(Console.ReadLine());
+
+            double total = 0;
+
+            switch (coin)
+            {
+                case 0.01:
+                    Console.WriteLine("1 cent");
+                    total += 0.01;
+                    break;
+
+                case 0.02:
+                    Console.WriteLine("2 cent");
+                    total += 0.02;
+                    break;
+
+                case 0.05:
+                    Console.WriteLine("5 cent");
+                    total += 0.05;
+                    break;
+
+                case 0.10:
+                    Console.WriteLine("10 cent");
+                    total += 0.10;
+                    break;
+
+                case 0.20:
+                    Console.WriteLine("20 cent");
+                    total += 0.20;
+                    break;
+
+                case 0.50:
+                    Console.WriteLine("50 cent");
+                    total += 0.50;
+                    break;
+
+                case 1.00:
+                    Console.WriteLine("€1");
+                    total += 0.20;
+                    break;
+
+                case 2.00:
+                    Console.WriteLine("€2");
+                    total += 0.50;
+                    break;
+            }
+        }
+    }
+}
---
+++ Code/CodeDemo2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace CodeDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coins are entered into device
+            // Reads size of coin
+
+            Console.WriteLine("Coin entered: ");
+            double[] coinArray = {0.20, 0.10, 1, 2, 0.50, 0.05, 0.02, 0.01, 1, 1 , 2, 2, 0.05, 0.02 };
+
+            double total = 0;
+
+            for (int i = 0; i < coinArray.Length; i++)
+            {
+                switch (coinArray[i])
+                {
+                    case 0.01:
+                        Console.WriteLine("1 cent");
+                        total += 0.01;
+                        break;
+
+                    case 0.02:
+                        Console.WriteLine("2 cent");
+                        total += 0.02;
+                        break;
+
+                    case 0.05:
+                        Console.WriteLine("5 cent");
+                        total += 0.05;
+                        break;
+
+                    case 0.10:
+                        Console.WriteLine("10 cent");
+                        total += 0.10;
+                        break;
+
+                    case 0.20:
+                        Console.WriteLine("20 cent");
+                        total += 0.20;
+                        break;
+
+                    case 0.50:
+                        Console.WriteLine("50 cent");
+                        total += 0.50;
+                        break;
+
+                    case 1.00:
+                        Console.WriteLine("€1");
+                        total += 0.20;
+                        break;
+
+                    case 2.00:
+                        Console.WriteLine("€2");
+                        total += 0.50;
+                        break;
+                }
+            }
+            Console.WriteLine($"Total = {total:C}");
+        }
+    }
+}
